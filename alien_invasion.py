c29541ac4e8a70dfc001cb12f1c699cd1025fd28
DefyingGravity10 <jeanneclarissetoledo@gmail.com>
2021-02-15 18:19:38 +0800

    Increased the game's speed each time a stage is cleared.

--- alien_invasion.py
+++ alien_invasion.py
@@ -64,9 +64,12 @@ class AlienInvasion:
         '''Start a new game when the player clicks "Play".'''
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            #Reset game settings.
+            self.settings.initialize_dynamic_settings()
             #Reset game statistics.
             self.stats.reset_stats()
             self.stats.game_active = True
+            
 
             #Get rid of remaining aliens and bullets.
             self.aliens.empty()
@@ -123,6 +126,7 @@ class AlienInvasion:
             #Destroys existing bullets, and creates a new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _create_fleet(self):
         '''Create the fleet of aliens.'''
--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ class Settings:
     '''Settings menu for Alien Invasion.'''
 
     def __init__(self):
-        '''Initialize the settings for the game.'''
+        '''Initialize the static settings for the game.'''
         #Screen settings
         self.screen_width = 1200
         self.screen_height = 800
@@ -14,7 +14,7 @@ class Settings:
 
         #Bullet settings
         self.bullet_speed = 1.6
-        self.bullet_width = 3.0
+        self.bullet_width = 800.0
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
@@ -22,6 +22,23 @@ class Settings:
         #Alien settings
         self.alien_speed = 1.0
         self.fleet_drop_speed = 10
+
+        #How quickly the game speeds up.
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        '''Initialize the settings that change throughout the game.'''
+        self.ship_speed = 1.5
+        self.bullet_speed = 1.6
+        self.alien_speed = 1.0
+
         #fleet direction of 1 represents right while -1 represents left.
         self.fleet_direction = 1
-        
+    
+    def increase_speed(self):
+        '''Increase speed settings.'''
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
